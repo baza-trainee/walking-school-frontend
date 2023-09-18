801e11b14bd788baaa61e3241ab6d8ed9ac3f3be
Kateryna Laina <boba4ok@gmail.com>
2023-09-18 17:46:45 +0300

    feature: create button component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.module.css";
+import Button from "./components/Button/Button";
 
 function App() {
-  return <div></div>;
+  return (
+    <div style={{ width: "173px" }}>
+      <Button
+        variant={"tertiary"}
+        disabled={false}
+        style={{ marginLeft: "100px", marginTop: "100px" }}
+      >
+        Підтримати
+      </Button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import style from "./button.module.scss";
+
+const Button = ({
+  variant = "large",
+  className,
+  children,
+  disabled,
+  ...props
+}) => {
+  const variantClass = {
+    large: style.large,
+    small: style.small,
+    tertiary: style.tertiary,
+  };
+
+  return (
+    <button
+      className={
+        `${variantClass[variant]} ${disabled ? " disabled" : ""}` +
+        " " +
+        className
+      }
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+      {variant === "tertiary" && disabled ? (
+        <img src="/images/btn_arrow_disabled.svg" alt={"arrow"} />
+      ) : (
+        <img src="/images/btn_arrow.svg" alt={"arrow"} />
+      )}
+    </button>
+  );
+};
+
+export default Button;
