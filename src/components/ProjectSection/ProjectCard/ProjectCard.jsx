b3af2f1ea66e9ff1672c2aa30eeefb3220690fdd
Kateryna Laina <boba4ok@gmail.com>
2023-09-19 15:30:29 +0300

    feature: card component done

--- src/components/ProjectSection/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectSection/ProjectCard/ProjectCard.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import style from "./card.module.css";
 import Link from "../../Links/Link";
+import { useMedia } from "../../../hooks/useMedia";
 
 const ProjectCard = () => {
+  const { isMobile, isTablet, isDesktop } = useMedia();
   return (
     <div className={style.card}>
       <div className={style.card__image}>
         <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0">
           <clipPath id="clip-path">
-            <path
-              d="M0 24C0 10.7452 10.7452 0 24 0H49.6032C55.1122 0 59.9005 3.78211 61.1765 9.14123C61.3914 10.044 61.5 10.9688 61.5 11.8968V16.447C61.5 29.7225 72.2775 40.4762 85.553 40.4469L263.947 40.0531C277.223 40.0238 288 50.7775 288 64.053V172C288 185.255 277.255 196 264 196H24C10.7452 196 0 185.255 0 172V24Z"
-              fill="#8D8BA7"
-            />
+            {isMobile ? (
+              <path
+                d="M0 24C0 10.7452 10.7452 0 24 0H49.6032C55.1122 0 59.9005 3.78211 61.1765 9.14123C61.3914 10.044 61.5 10.9688 61.5 11.8968V16.447C61.5 29.7225 72.2775 40.4762 85.553 40.4469L263.947 40.0531C277.223 40.0238 288 50.7775 288 64.053V172C288 185.255 277.255 196 264 196H24C10.7452 196 0 185.255 0 172V24Z"
+                fill="#8D8BA7"
+              />
+            ) : isTablet ? (
+              <path
+                d="M0 24C0 10.7452 10.7452 0 24 0H114.506C121.41 0 127.006 5.59644 127.006 12.5V16.1562C127.006 29.4111 137.751 40.1562 151.006 40.1562H328C341.255 40.1562 352 50.9014 352 64.1562V231C352 244.255 341.255 255 328 255H24C10.7452 255 0 244.255 0 231V24Z"
+                fill="#8D8BA7"
+              />
+            ) : (
+              <path
+                d="M0 24C0 10.7452 10.7452 0 24 0H114.5C118.918 0 122.5 3.58172 122.5 8L121.815 13.5683C120.053 27.878 131.217 40.5 145.635 40.5H343C356.255 40.5 367 51.2452 367 64.5V264C367 277.255 356.255 288 343 288H24C10.7452 288 0 277.255 0 264V24Z"
+                fill="#8D8BA7"
+              />
+            )}
           </clipPath>
         </svg>
         <img
@@ -20,70 +34,14 @@ const ProjectCard = () => {
         />
       </div>
       <div className={style.date}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-        >
-          <path
-            d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z"
-            stroke="black"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M16 2V6"
-            stroke="black"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M8 2V6"
-            stroke="black"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M3 10H21"
-            stroke="black"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
+        <img src="/images/calendar.svg" alt="calendar" />
         вересень - листопад
       </div>
       <div className={style.info}>
         <div className={style.data}>
           <h3>FlySport</h3>
           <div className={style.data__age}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <path
-                d="M19 21V19C19 17.9391 18.5786 16.9217 17.8284 16.1716C17.0783 15.4214 16.0609 15 15 15H9C7.93913 15 6.92172 15.4214 6.17157 16.1716C5.42143 16.9217 5 17.9391 5 19V21"
-                stroke="black"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z"
-                stroke="black"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <img src="/images/age.svg" alt="age" />
             <p>18-60 років</p>
           </div>
           <div>
@@ -94,7 +52,7 @@ const ProjectCard = () => {
           </div>
         </div>
         <div className={style.join}>
-          <Link to={"/dfdfdfdf"} variant={"small"}>
+          <Link to={"/dfdfdfdf"} variant={"small"} className={style.join__link}>
             Взяти участь
           </Link>
         </div>
