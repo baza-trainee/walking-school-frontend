aec88b9d0c368193cf20c8a619f6ea82951f33dd
dsdyaz <dsdy06@gmail.com>
2023-09-19 10:57:45 +0300

    chore: add docs to adventure-block component

--- src/components/adventure-blocks/adventure-block/adventure-block.jsx
+++ src/components/adventure-blocks/adventure-block/adventure-block.jsx
@@ -1,8 +1,21 @@
 import React from "react"
+import PropTypes from "prop-types"
 import style from "./adventure-block.module.css"
 
+/**
+ * Component card for representing some activity included in the project
+ * @component
+ * @example
+ * const image = "pathToImg/img"
+ * const altText = "a man doing bungee jumping"
+ * const text = "bungee jumping"
+ * return (
+ *     <AdventureBlock imageSrc={image} imageAlt={altText} text={text} />
+ * )
+ * @returns 
+ */
 export default function AdventureBlock(props) {
-    const {imageSrc, imageAlt,  text} = props
+    const {imageSrc, imageAlt, text} = props
     return (
         <div className={style.card}>
             <div className={style.cardContent}>
@@ -13,4 +26,19 @@ export default function AdventureBlock(props) {
             </div>
         </div>
     )
+}
+
+AdventureBlock.propTypes = {
+    /**
+     * source of the displayed image
+     */
+    imageSrc: PropTypes.string.isRequired,
+    /**
+     * alternative description of displayed image
+     */
+    imageAlt: PropTypes.string.isRequired,
+    /**
+     * displayed text, name of the displayed activity
+     */
+    text: PropTypes.string.isRequired
 }
