6a06937d9e3156dd18a88652750c0b199dd944ba
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-09-19 15:03:17 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header/Header";
-import Button from "./components/Button/Button";
+// import Button from "./components/Button/Button";
 import { Scroller } from "./components/Scroller/Scroller";
 import { Offerings } from "./components/Offerings/Offerings";
 import "./App.module.css";
@@ -7,9 +7,9 @@ import "./App.module.css";
 function App() {
   return (
     <div>
-      <Scroller />
       <Header />
       <Offerings />
+      <Scroller />
     </div>
   );
 }
