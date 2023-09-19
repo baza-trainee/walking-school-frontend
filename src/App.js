c617b6ac62f68c43d2077e047d747bf71d1a31a0
Denys Topchyi <90519083+Blenemy@users.noreply.github.com>
2023-09-19 14:58:22 +0300

    Feature/no ref/create new offerings component (#2)
    * feat: add new title template component
    
    * feat: add new offerings component
    
    * refactor: delete custom reset properties
    
    * fixed bugs
    
    ---------
    
    Co-authored-by: OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from "./components/Header/Header";
 import Button from "./components/Button/Button";
-import "./App.module.css";
 import { Scroller } from "./components/Scroller/Scroller";
+import { Offerings } from "./components/Offerings/Offerings";
+import "./App.module.css";
 
 function App() {
   return (
     <div>
       <Scroller />
       <Header />
+      <Offerings />
     </div>
   );
 }
