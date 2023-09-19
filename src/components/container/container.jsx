feec732a45807c9757f415b968d806b67e350957
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-09-19 15:40:37 +0300

    fixed bugs

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import style from "./container.module.css";
+import style from "./Container.module.css";
 
 /**
  * A wrapper for page content
@@ -8,15 +8,8 @@ import style from "./container.module.css";
  * @returns {React.JSX.Element} react element wrapped inside if this component
  */
 export default function Container(props) {
-  const { children, leftRightPadding, topBottomPadding } = props;
-  const horizontalPadding = `${leftRightPadding}%`;
-  const verticalPadding = `${topBottomPadding}%`;
-  const padding = verticalPadding + horizontalPadding;
-  return (
-    <div style={{ padding: padding }} className={style.container}>
-      {children}
-    </div>
-  );
+  const { children } = props;
+  return <div className={style.container}>{children}</div>;
 }
 
 Container.propTypes = {
@@ -24,17 +17,4 @@ Container.propTypes = {
    * a content of the container
    */
   children: PropTypes.elementType.isRequired,
-  /**
-   * padding size on the right and left sides in %, default equals to 0
-   */
-  leftRightPadding: PropTypes.number,
-  /**
-   * padding size on top and bottom in %, default equals to 0
-   */
-  topBottomPadding: PropTypes.number,
-};
-
-Container.defaultProps = {
-  leftRightPadding: 0,
-  topBottomPadding: 0,
 };
