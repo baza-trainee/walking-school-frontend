6425aa4cfc1d60aa3b49d1227828a3b32052911f
Kateryna Laina <boba4ok@gmail.com>
2023-09-20 15:24:18 +0300

    hotfix: fix arrow displaying in carousel

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -8,6 +8,7 @@ import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const ProjectSection = () => {
   const { isMobile, isTablet, isDesktop } = useMedia();
