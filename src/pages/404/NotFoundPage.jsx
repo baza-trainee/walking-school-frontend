fc7a48a8d26dfe39c422ecc4fa01e3080c8285ec
dsdyaz <dsdy06@gmail.com>
2023-09-20 21:34:23 +0300

    feat: create 404 page

--- src/pages/404/NotFoundPage.jsx
+++ src/pages/404/NotFoundPage.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import Container from "../../components/layout/Container/Container";
 import Button from "../../components/UI/Button/Button";
+import Image from "../../assets/images/404.svg";
 import style from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
   return (
     <Container>
       <section className={style.notFound}>
-        <div className={style.notFound__content}>
-          <span className={style.notFound__number}>
-            404
-          </span>
+        <img src={Image} alt="not found" className={style.notFound__image} />
+        <div className={style.notFound__main}>
           <div className={style.notFound__text}>
-            <span className={style.notFound__mainMessage}>
-              Сторінка не знайдена
-            </span>
-            <span className={style.notFound__secondaryMessage}>
-              Ми не змогли знайти сторінку, яку Ви шукаєте. Можливо, сталася помилка
-            </span>
-          </div>          
-          <Button className={style.returnButton} type="button">Повернутись на головну</Button>
+            <span className={style.notFound__number}>404</span>
+            <div className={style.message}>
+              <span className={style.message__primary}>
+                Сторінка не знайдена
+              </span>
+              <span className={style.message__secondary}>
+                Ми не змогли знайти сторінку, яку Ви шукаєте. Можливо, сталася
+                помилка
+              </span>
+            </div>
+          </div>
+          <Button className={style.button} type="button">
+            Повернутись на головну
+          </Button>
         </div>
       </section>
     </Container>
-  )
+  );
 };
 
 export default NotFoundPage;
