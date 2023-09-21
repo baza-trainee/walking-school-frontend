fdec39baa925e9e1fc3f224a3dd3cf0e16256a79
dsdyaz <dsdy06@gmail.com>
2023-09-21 22:48:49 +0300

    feat: add Requirements element(work in progress)

---
+++ src/components/Requirements/Requirements.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Requirement from "./RequirementsElement/RequirementsElement";
+import Container from "../layout/Container/Container";
+import style from "./requirements.module.css";
+
+const listItems = [
+  { text: "Бажання" },
+  { text: "Біометричний закордонний паспорт" },
+  { text: "Оформлена група інвалідності(для виїзду закордон)" },
+  { text: "У тебе вже має бути протез" },
+  { text: "Твоя вага має бути меншою за 95кг" },
+  {
+    text: "Відсутність протипоказань щодо вказаних вище екстримальних заходів",
+  },
+];
+
+const listElements = listItems.map((element) => (
+  <Requirement text={element.text} key={element.text} />
+));
+
+const Requirements = () => {
+  return (
+    <Container>
+      <section className={style.requirements}>
+        <h2>Основні вимоги</h2>
+        <img src="https://picsum.photos/200" alt="insert alt" />
+        <div className={style.requirements__list}>{listElements}</div>
+      </section>
+    </Container>
+  );
+};
+
+export default Requirements;
