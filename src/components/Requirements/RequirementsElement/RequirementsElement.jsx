7eaeaee5a0e33c496b6d92ecf3036749e2eb0636
dsdyaz <dsdy06@gmail.com>
2023-09-21 08:52:26 +0300

    feat: create base for single Requirement component

---
+++ src/components/Requirements/RequirementsElement/RequirementsElement.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Check from "../../../assets/icons/checkCircle.svg";
+import style from "./requirementsElement.module.css";
+
+const Requirement = (props) => {
+  const { text } = props;
+  return (
+    <div className={style.requirement}>
+      <img className={style.requirement__icon} src={Check} alt="" />
+      <span className={style.requirement__text}>{text}</span>
+    </div>
+  );
+};
+
+Requirement.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Requirement;
