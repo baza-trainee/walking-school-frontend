e0b52789b29e83513403e517b310e57835f6961d
Denys Topchyi <90519083+Blenemy@users.noreply.github.com>
2023-09-21 11:31:13 +0300

    feat: add footer component (#21)
    Co-authored-by: Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Main from "./components/layout/Main/Main";
 // import NotFoundPage from "./pages/404/NotFoundPage";
 
 import "./App.module.css";
+import { Footer } from "./components/layout/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Main />
-      {/* <NotFoundPage /> */}
+      <Footer />
+{/* <NotFoundPage /> */}
     </div>
   );
 }
