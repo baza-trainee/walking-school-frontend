b335b9608797d36a697cbe1f9e429d76df694d6d
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-09-21 16:09:27 +0300

    only Advantures

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import Header from "./components/layout/Header/Header";
+// import Header from "./components/layout/Header/Header";
 import Main from "./components/layout/Main/Main";
 // import NotFoundPage from "./pages/404/NotFoundPage";
 
 import "./App.module.css";
-import { Footer } from "./components/layout/Footer/Footer";
+// import { Footer } from "./components/layout/Footer/Footer";
 
 function App() {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <Main />
-      <Footer />
-{/* <NotFoundPage /> */}
+      {/* <Footer /> */}
+      {/* <NotFoundPage /> */}
     </div>
   );
 }
