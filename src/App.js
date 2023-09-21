fc7e7e842bbc2ded60d543b26b5ac9e53f6b4bae
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-09-21 17:28:36 +0300

    update gh-pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-// import Header from "./components/layout/Header/Header";
+import Header from "./components/layout/Header/Header";
 import Main from "./components/layout/Main/Main";
 // import NotFoundPage from "./pages/404/NotFoundPage";
 
 import "./App.module.css";
-// import { Footer } from "./components/layout/Footer/Footer";
+import { Footer } from "./components/layout/Footer/Footer";
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
+      <Header />
       <Main />
-      {/* <Footer /> */}
+      <Footer />
       {/* <NotFoundPage /> */}
     </div>
   );
--- src/components/layout/Main/Main.jsx
+++ src/components/layout/Main/Main.jsx
@@ -1,22 +1,22 @@
 import Adventures from "../../Adventures";
-// import { Scroller } from "../../Scroller/Scroller";
-// import { Offerings } from "../../Offerings/Offerings";
-// import ProjectSection from "../../ProjectSection/ProjectSection";
-// import { Contact } from "../../Contact/Contact";
-// import { Partners } from "../../Partners/Partners";
-// import ParticipateSection from "../../ParticipateSection/ParticipateSection";
+import { Scroller } from "../../Scroller/Scroller";
+import { Offerings } from "../../Offerings/Offerings";
+import ProjectSection from "../../ProjectSection/ProjectSection";
+import { Contact } from "../../Contact/Contact";
+import { Partners } from "../../Partners/Partners";
+import ParticipateSection from "../../ParticipateSection/ParticipateSection";
 import styles from "./Main.module.css";
 
 const Main = () => (
   // eslint-disable-next-line react/prop-types
   <main className={styles.main}>
     <Adventures />
-    {/* <Offerings /> */}
-    {/* <Scroller /> */}
-    {/* <ProjectSection /> */}
-    {/* <Partners /> */}
-    {/* <ParticipateSection /> */}
-    {/* <Contact /> */}
+    <Offerings />
+    <Scroller />
+    <ProjectSection />
+    <Partners />
+    <ParticipateSection />
+    <Contact />
   </main>
 );
 
