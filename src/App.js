4eba8dc4189a08ff4f040fef6264d44bede36c2d
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-09-21 09:48:34 +0300

    add 404 to app.js for checking

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Header from "./components/layout/Header/Header";
 import Main from "./components/layout/Main/Main";
+// import NotFoundPage from "./pages/404/NotFoundPage";
 
 import "./App.module.css";
-import { Contact } from "./components/Contact/Contact";
 
 function App() {
   return (
     <div>
       <Header />
       <Main />
+      {/* <NotFoundPage /> */}
     </div>
   );
 }
