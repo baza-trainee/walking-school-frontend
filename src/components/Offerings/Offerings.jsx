5d22408534f501b4d69b9e94975880992d3c23e9
Denys Topchyi <miliarder97@gmail.com>
2023-09-22 17:14:14 +0300

    feat: add responsive to the block

--- src/components/Offerings/Offerings.jsx
+++ src/components/Offerings/Offerings.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import fillerImage from "../../assets/images/Image.png";
+import bannerMobile from "../../assets/images/offeringsBannerMobile.png";
+import bannerTablet from "../../assets/images/offeringsBanner.png";
+import bannerDesktop from "../../assets/images/offeringsBannerDesktop.png";
 import styles from "./Offerings.module.css";
 import { TitleTemplate } from "../TitleTemplate/TitleTemplate";
 import { OfferingsList } from "./OfferingsList/OfferingsList";
+import Container from "../layout/Container";
+import { useMedia } from "../../hooks/useMedia";
 
 export const Offerings = () => {
+  const { isMobile, isTablet } = useMedia();
+  const imageMap = {
+    mobile: bannerMobile,
+    tablet: bannerTablet,
+    desktop: bannerDesktop,
+  };
+
+  const imageSrc = isMobile
+    ? imageMap.mobile
+    : isTablet
+    ? imageMap.tablet
+    : imageMap.desktop;
+
   const offeringsList = [
     "Фізичне відновлення",
     "Подолання власних фізичних обмежень",
@@ -17,22 +34,29 @@ export const Offerings = () => {
   ];
 
   return (
-    <section className={styles.Offerings}>
-      <div className="offerings__container">
+    <section className={styles.offerings}>
+      <Container>
         <div className={styles.content}>
           <TitleTemplate
             title="Що ви отримаєте"
-            subtitle="Take part in the various events you want with ease with an extraordinary experience with Coco."
+            subtitle="Спробую навчити Вас ходити у незвичних умовах, та всьому що сам знаю. Ми вчитемося ходити у Польських Татрах"
+            className={styles.customTitle}
           />
-          <div className="offerings__image">
-            <img src={fillerImage} alt="Offerings" className="offerings__img" />
+          <div className={styles.image}>
+            <img src={imageSrc} alt="Offerings" className={styles.img} />
           </div>
-          <div>
-            <OfferingsList list={offeringsList.slice(0, 4)} />
-            <OfferingsList list={offeringsList.slice(4)} />
+          <div className={styles.listContainer}>
+            <OfferingsList
+              list={offeringsList.slice(0, 4)}
+              className={styles.list}
+            />
+            <OfferingsList
+              list={offeringsList.slice(4)}
+              className={styles.list}
+            />
           </div>
         </div>
-      </div>
+      </Container>
     </section>
   );
 };
