b0a7bd4cd1f86a01809f143e41283c1e39819c41
Kateryna Lanina <124586601+lanina90@users.noreply.github.com>
2023-09-23 13:18:35 +0300

    feature: unit-tests for Participate ans Steps container (#49)
    * feature: unit-tests for Participate ans Steps container
    
    * feature: unit-tests for Participate ans Steps container

--- src/components/Steps/Steps.jsx
+++ src/components/Steps/Steps.jsx
@@ -4,10 +4,11 @@ import style from "./steps.module.css";
 const Steps = ({ steps, labels }) => {
   const stepArray = Array.from({ length: steps }, (_, i) => i + 1);
   return (
-    <div className={style["steps-container"]}>
+    <div data-testid={"steps-container"} className={style["steps-container"]}>
       <div className={style["progress-line"]}></div>
       {stepArray.map((step, index) => (
         <div
+          data-testid="step"
           key={index}
           className={style.step}
           style={{ animationDelay: `${0.9 * (index + 1)}s` }}
