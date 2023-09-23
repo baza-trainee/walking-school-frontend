e124973a174c795a13f857d5342c86cb5cabbbc5
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2023-09-23 13:19:05 +0300

    Feature/card#29/create main requirements (#48)
    * chore: download icon
    
    * feat: create base for single Requirement component
    
    * feat: finish requirementsElement component
    
    * feat: add Requirements element(work in progress)
    
    * feat: make requirements element for mobile
    (in progress)
    
    * chore: add index.jsx for requirements
    
    * chore: delete index.jsx file for requirements
    
    * chore: rename module.css files to be in pascalCase
    
    * feat: update css for tablet screen
    
    * chore: download images
    
    * feat: add adaptivity to Requirements component
    
    ---------
    
    Co-authored-by: dsdyaz <dsdy06@gmail.com>

--- src/components/Requirements/RequirementsElement/RequirementsElement.jsx
+++ src/components/Requirements/RequirementsElement/RequirementsElement.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Check from "../../../assets/icons/checkCircle.svg";
-import style from "./requirementsElement.module.css";
+import style from "./RequirementsElement.module.css";
 
 const Requirement = (props) => {
   const { text } = props;
   return (
     <div className={style.requirement}>
       <img className={style.requirement__icon} src={Check} alt="" />
-      <div className={style.requirement__text}>
-        <span>{text}</span>
-      </div>
+      <span className={style.requirement__text}>{text}</span>
     </div>
   );
 };
