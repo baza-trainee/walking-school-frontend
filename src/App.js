cef13a3c9b5e3becc3d16285b140fd6d000af91c
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-09-24 14:07:40 +0300

    Update for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import Header from "./components/layout/Header/Header";
 import Main from "./components/layout/Main/Main";
+import { Footer } from "./components/layout/Footer/Footer";
 
 import "./App.module.css";
-import { Footer } from "./components/layout/Footer/Footer";
 
 function App() {
   return (
--- src/components/layout/Main/Main.jsx
+++ src/components/layout/Main/Main.jsx
@@ -2,8 +2,8 @@
 import { Scroller } from "../../Scroller/Scroller";
 import { Offerings } from "../../Offerings/Offerings";
 // import Projects from "../../Projects/Projects";
-import { Contact } from "../../Contact/Contact";
-import Requirements from "../../Requirements/Requirements";
+// import { Contact } from "../../Contact/Contact";
+// import Requirements from "../../Requirements/Requirements";
 import Participate from "../../Participate/Participate";
 // import { FollowUsFacebook } from "../../FollowUsFacebook/index.jsx";
 // import HeroSection from "../../HeroSection/index";
@@ -19,9 +19,9 @@ const Main = () => (
     {/* <Projects /> */}
     {/* <Partners /> */}
     <Participate />
-    <Requirements />
+    {/* <Requirements /> */}
     {/* <FollowUsFacebook /> */}
-    <Contact />
+    {/* <Contact /> */}
   </main>
 );
 
