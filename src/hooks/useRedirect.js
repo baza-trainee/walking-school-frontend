da90b431ff703781075aa76d99b18e7db567e130
dsdyaz <dsdy06@gmail.com>
2023-09-26 14:26:52 +0300

    chore: add docs to useRedirect

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -1,5 +1,15 @@
 import { useNavigate } from "react-router-dom";
 
+/**
+ * @description hook used to redirect to another page
+ *
+ * @example
+ * const redirect = userRedirect("your path")
+ *
+ * <Button onCLick={redirect}>
+ *   Redirect button
+ * </Button>
+ */
 export default function useRedirect(path) {
   const navigate = useNavigate();
   return () => navigate(path);
