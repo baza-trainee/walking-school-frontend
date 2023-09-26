fb14aafee6d5d79e5ddfda3aa2a026629375584f
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2023-09-26 08:47:37 +0200

    Feature/card#50/unit test to 404 (#74)
    * feat: add bg-color to 404 page
    
    * feat: add tests to NotFoundPage
    
    ---------
    
    Co-authored-by: dsdyaz <dsdy06@gmail.com>

---
+++ src/pages/404/__test__/NotFoundPage.test.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import NotFoundPage from "../NotFoundPage";
+
+afterEach(cleanup);
+
+describe("404 page", () => {
+  it("renders", () => {
+    render(<NotFoundPage />);
+    const page = screen.getByTestId("page");
+    expect(page).toBeInTheDocument();
+  });
+
+  it("is visible", () => {
+    render(<NotFoundPage />);
+    const page = screen.getByTestId("page");
+    expect(page).toBeVisible();
+  });
+
+  it("isn't empty", () => {
+    render(<NotFoundPage />);
+    const page = screen.getByTestId("page");
+    expect(page).not.toBeEmptyDOMElement();
+  });
+
+  it("contains an image", () => {
+    render(<NotFoundPage />);
+    const page = screen.getByTestId("page");
+    const image = screen.getByAltText("not found");
+    expect(page).toContainElement(image);
+  });
+
+  it("contains a message", () => {
+    render(<NotFoundPage />);
+    const page = screen.getByTestId("page");
+    const text404 = screen.getByText("404");
+    const pageNotFound = screen.getByText("Сторінка не знайдена");
+    const couldntFindAPage = screen.getByText(
+      "Ми не змогли знайти сторінку, яку Ви шукаєте. Можливо, сталася помилка",
+    );
+    expect(page).toContainElement(text404);
+    expect(page).toContainElement(pageNotFound);
+    expect(page).toContainElement(couldntFindAPage);
+  });
+
+  it("contains a button", () => {
+    render(<NotFoundPage />);
+    const page = screen.getByTestId("page");
+    const button = screen.getByRole("button");
+    expect(page).toContainElement(button);
+  });
+});
