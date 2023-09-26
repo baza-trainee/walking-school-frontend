5703f4a6ec59269ec2ffab580c5e486b8213737e
dsdyaz <dsdy06@gmail.com>
2023-09-26 13:55:04 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import Header from "./components/layout/Header/Header";
 import Main from "./components/layout/Main/Main";
 import { Footer } from "./components/layout/Footer/Footer";
+import NotFoundPage from "./pages/404/NotFoundPage";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 import "./App.module.css";
+import Layout from "./components/layout/Layout/Layout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index path="walking-school-frontend" element={<Main />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>,
+  )
+);
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-      {/* <NotFoundPage /> */}
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
