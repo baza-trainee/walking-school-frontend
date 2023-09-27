b0835bf3d392a9a27b7081773768db934143313b
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-09-27 13:54:24 +0300

    Feature/no ref/create slider (#81)
    * fix: fixing slider
    
    * fix: change styles
    
    * fix: styles to slider

--- src/components/ScrollUp/__test__/Scrollup.test.js
+++ src/components/ScrollUp/__test__/Scrollup.test.js
@@ -1,23 +1,22 @@
 import { screen, render, cleanup, fireEvent } from "@testing-library/react";
 import ScrollUp from "../ScrollUp";
 
-afterEach(cleanup)
+afterEach(cleanup);
 
 const scrollToSpy = jest.fn();
 global.scrollTo = scrollToSpy;
 
 describe("scroll-up button", () => {
   it("renders", () => {
-    render(<ScrollUp />)
-    const scrollUp = screen.getByRole("button")
-    expect(scrollUp).toBeInTheDocument()
-  })
+    render(<ScrollUp />);
+    const scrollUp = screen.getByRole("button");
+    expect(scrollUp).toBeInTheDocument();
+  });
 
   it("is calling scroll when clicked", () => {
-    render(<ScrollUp />)
-    const scrollUp = screen.getByTestId("scroll")
-    fireEvent.click(scrollUp)
-    expect(scrollToSpy).toBeCalled()
-  })
-
-})
+    render(<ScrollUp />);
+    const scrollUp = screen.getByTestId("scroll");
+    fireEvent.click(scrollUp);
+    expect(scrollToSpy).toBeCalled();
+  });
+});
