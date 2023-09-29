233db364516e8aead6bc4abcf4a13bac02c4577d
dsdyaz <dsdy06@gmail.com>
2023-09-29 22:02:37 +0300

    feat: update close button to support external prop

--- src/components/AdminPanel/Header/Close/Close.jsx
+++ src/components/AdminPanel/Header/Close/Close.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { ReactComponent as Icon } from "../../../../assets/admin/closeWhite.svg";
 import style from "./Close.module.css";
 
@@ -8,27 +7,16 @@ import style from "./Close.module.css";
  *
  * @returns {React.JSX.Element} Close button of specified state
  */
-const Close = (props) => {
-  const { isSecondary, isDisabled } = props;
+const Close = ({ isSecondary = false, isDisabled = false, ...props }) => {
   const classes = [style.close];
   if (isSecondary) classes.push(style.close_secondary);
   if (isDisabled) classes.push(style.close_disabled);
   const className = classes.join(" ");
   return (
-    <button className={className}>
+    <button className={className} {...props}>
       <Icon />
     </button>
   );
-};
-
-Close.propTypes = {
-  isSecondary: PropTypes.bool,
-  isDisabled: PropTypes.bool,
-};
-
-Close.defaultProps = {
-  isSecondary: false,
-  isDisabled: false,
 };
 
 export default Close;
