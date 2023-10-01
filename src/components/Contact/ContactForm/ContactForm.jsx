a94a2d7745765145a4a6be1c30243385126fcd9d
Denys Topchyi <90519083+Blenemy@users.noreply.github.com>
2023-10-01 14:18:24 +0300

    Feature/issue 33/create new contact component (#98)
    * feat: add new contact component
    
    * fix: adjust label with input, add state reset
    
    * fix: add responsive to form and title component
    
    * fix: improve responsive layout, add vars
    
    * feat: add tests to contact component
    
    * fix: fix bug
    
    * feat: add modal comp
    
    * feat: add validation for the name input
    
    ---------
    
    Co-authored-by: Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>

--- src/components/Contact/ContactForm/ContactForm.jsx
+++ src/components/Contact/ContactForm/ContactForm.jsx
@@ -1,72 +1,49 @@
 import { useState } from "react";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 import Button from "../../UI/Button/Button";
 import { CustomInput } from "../CustomInput/CustomInput";
 import styles from "./ContactForm.module.css";
 import { Modal } from "../../Modal/Modal";
 
+const validationSchema = Yup.object({
+  username: Yup.string()
+    .min(2, "Ім’я повинно бути не менше 2 знаків")
+    .max(50, "Ім’я повинно бути не більше 50 знаків")
+    .required("Введіть ім’я"),
+  surname: Yup.string().required("Введіть своє прізвище"),
+  email: Yup.string()
+    .email("Невірна електронна пошта")
+    .required("Введіть електронну пошту"),
+  phoneNumber: Yup.string(),
+  message: Yup.string(),
+});
+
 export const ContactForm = () => {
-  // TODO: create a custom hook and move the form logic there
-  const [formData, setFormData] = useState({
-    username: "",
-    surname: "",
-    email: "",
-    phoneNumber: "",
-    message: "",
-  });
-  const [formErrors, setFormErrors] = useState({
-    username: null,
-    surname: null,
-    email: null,
-    phoneNumber: "",
-    message: "",
-  });
   const [isActive, setIsActive] = useState(false);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const errors = {
-      username: !formData.username ? "Введіть своє ім’я" : null,
-      surname: !formData.surname ? "Введіть своє прізвище" : null,
-      email: !formData.email ? "Введіть електронну пошту" : null,
-    };
-
-    if (errors.username || errors.email || errors.surname) {
-      setFormErrors(errors);
-      return;
-    }
-
-    setFormData({
+  const formik = useFormik({
+    initialValues: {
       username: "",
       surname: "",
       email: "",
       phoneNumber: "",
       message: "",
-    });
-    setFormErrors({
-      username: "",
-      surname: "",
-      email: "",
-      phoneNumber: "",
-      message: "",
-    });
-    setIsActive(true);
-  };
-
-  const handleInputChange = (event, setAction) => {
-    const { name, value } = event.target;
-    setAction((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+    },
+    validationSchema,
+    validateOnBlur: true,
+    onSubmit: (values, { resetForm }) => {
+      setIsActive(true);
+      resetForm();
+    },
+  });
 
   return (
     <form
       data-testid="form"
       action=""
       className={styles.form}
-      onSubmit={handleSubmit}
+      onSubmit={formik.handleSubmit}
     >
       <div className={styles.wrapper}>
         <CustomInput
@@ -74,9 +51,10 @@ export const ContactForm = () => {
           placeholder="Введіть своє ім’я"
           text="Ім’я"
           name="username"
-          onChangeHandler={(event) => handleInputChange(event, setFormData)}
-          value={formData.username}
-          error={formErrors.username}
+          onChangeHandler={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.username}
+          error={formik.touched.username && formik.errors.username}
           required
         />
         <CustomInput
@@ -84,9 +62,10 @@ export const ContactForm = () => {
           placeholder="Введіть своє прізвище"
           text="Прізвище"
           name="surname"
-          onChangeHandler={(event) => handleInputChange(event, setFormData)}
-          value={formData.surname}
-          error={formErrors.surname}
+          onChangeHandler={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.surname}
+          error={formik.touched.surname && formik.errors.surname}
           required
         />
         <CustomInput
@@ -94,34 +73,39 @@ export const ContactForm = () => {
           placeholder="Введіть електронну пошту"
           text="Email"
           name="email"
-          onChangeHandler={(event) => handleInputChange(event, setFormData)}
-          value={formData.email}
-          error={formErrors.email}
+          onChangeHandler={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+          error={formik.touched.email && formik.errors.email}
           required
         />
-
         <CustomInput
           type="tel"
           placeholder="+XXX -"
           text="Телефон"
           name="phoneNumber"
-          onChangeHandler={(event) => handleInputChange(event, setFormData)}
-          value={formData.phoneNumber}
-          error={formErrors.phoneNumber}
+          onChangeHandler={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.phoneNumber}
+          error={formik.touched.phoneNumber && formik.errors.phoneNumber}
         />
       </div>
 
       <CustomInput
         type="textarea"
         placeholder="Введіть ваше повідомлення"
+        text="Повідомлення"
         name="message"
-        onChangeHandler={(event) => handleInputChange(event, setFormData)}
-        value={formData.message}
-        error={formErrors.message}
+        onChangeHandler={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.message}
+        error={formik.touched.message && formik.errors.message}
       />
+
       <Button className={styles.button} variant="large" type="submit">
         Надіслати
       </Button>
+
       {isActive && <Modal setIsActive={setIsActive} />}
     </form>
   );
