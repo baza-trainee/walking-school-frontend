1cc07811c7b5adf796796af8dc93a1781e2776f7
dsdyaz <dsdy06@gmail.com>
2023-10-01 22:21:02 +0300

    feat: create reusable header

--- src/components/AdminPanel/Header/AdminHeader.jsx
+++ src/components/AdminPanel/Header/AdminHeader.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Close from "./Close/Close";
 import Search from "./Search/Search";
-import PropTypes from "prop-types";
 
 import style from "./AdminHeader.module.css";
 import AdminButton from "../UI/Button/AdminButton";
@@ -23,19 +22,19 @@ const AdminHeader = (props) => {
   const { withSearch, withButton, withClose, heading } = props;
   return (
     <header className={style.header}>
-      <div className={style.header__heading}>{heading}</div>
+      <h4 className={style.header__heading}>{heading}</h4>
       <div className={style.header__content}>
         {withSearch ? <Search /> : null}
         {withButton ? (
           <AdminButton
             variant="primary"
-            tabindex="0"
+            tabIndex="0"
             className={style.header__button}
           >
             Додати
           </AdminButton>
         ) : null}
-        {withClose ? <Close tabindex="0" /> : null}
+        {withClose ? <Close tabIndex="0" /> : null}
       </div>
     </header>
   );
