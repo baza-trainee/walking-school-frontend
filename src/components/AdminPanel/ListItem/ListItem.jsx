352bff546c7c0f381727fff31b6241af2a674b93
dsdyaz <dsdy06@gmail.com>
2023-10-02 16:21:59 +0300

    feat: create listItem component

--- src/components/AdminPanel/ListItem/ListItem.jsx
+++ src/components/AdminPanel/ListItem/ListItem.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { ReactComponent as EditIcon } from "../../../assets/admin/pencil.svg";
+import { ReactComponent as DeleteIcon } from "../../../assets/admin/trashbin.svg";
 
 import style from "./ListItem.module.css";
 
@@ -19,7 +21,14 @@ const ListItem = ({
         ) : null}
         <div className={style.listItem__date}>{date}</div>
       </div>
-      <div className={style.listItem__buttons}></div>
+      <div className={style.listItem__buttons}>
+        <div className={style.button} onClick={navigateToEdit} role="button">
+          <EditIcon />
+        </div>
+        <div className={style.button} onClick={deleteFunc} role="button">
+          <DeleteIcon />
+        </div>
+      </div>
     </div>
   );
 };
