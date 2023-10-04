3ea5b8360c6441b3b0211289c908ce118d83079d
dsdyaz <dsdy06@gmail.com>
2023-10-04 12:44:04 +0300

    chore: format the file

--- src/pages/AdminPanel/AdminPartners/AddPartner/AddPartner.jsx
+++ src/pages/AdminPanel/AdminPartners/AddPartner/AddPartner.jsx
@@ -14,35 +14,52 @@ const AddPartner = () => {
     navigate("/admin/partners");
   };
 
-  const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState("");
   const inputChange = (event) => {
-    setInputValue(event.target.value)
-    console.log(inputValue)
-  }
+    setInputValue(event.target.value);
+    console.log(inputValue);
+  };
 
-  const [imageValue, setImageValue] = useState('')
+  const [imageValue, setImageValue] = useState("");
   const imageChange = (newPreview) => {
-    setImageValue(newPreview)
-    console.log(imageValue)
+    setImageValue(newPreview);
+    console.log(imageValue);
   };
 
   const submitFunc = (event) => {
-    event.preventDefault()
-    console.log(imageValue)
-    console.log(inputValue)
-  }
+    event.preventDefault();
+    console.log(imageValue);
+    console.log(inputValue);
+  };
 
   return (
     <div className={style.page}>
       <AdminHeader withClose closeFunc={closeFunc} heading="Додати партнера" />
       <form onSubmit={submitFunc} className={style.page__content}>
         <div className={style.inputs}>
-        <AdminInput value={inputValue} onChange={inputChange} variant="admin" placeholder="Назва"/>
-        <ImageInput value={imageValue} onChange={imageChange} variant="project"/>
+          <AdminInput
+            value={inputValue}
+            onChange={inputChange}
+            variant="admin"
+            placeholder="Назва"
+          />
+          <ImageInput
+            value={imageValue}
+            onChange={imageChange}
+            variant="project"
+          />
         </div>
         <div className={style.buttons}>
-          <AdminButton style={{"width": "196px"}} variant="secondary">Скасувати</AdminButton>
-          <AdminButton style={{"width": "196px"}} type="submit" variant="primary">Додати</AdminButton>
+          <AdminButton style={{ width: "196px" }} variant="secondary">
+            Скасувати
+          </AdminButton>
+          <AdminButton
+            style={{ width: "196px" }}
+            type="submit"
+            variant="primary"
+          >
+            Додати
+          </AdminButton>
         </div>
       </form>
     </div>
