f957ca715441ef4a0d0300c21e04dfefa188756b
dsdyaz <dsdy06@gmail.com>
2023-10-04 12:46:02 +0300

    feat: add form logic to EditPartner

--- src/pages/AdminPanel/AdminPartners/EditPartner/EditPartner.jsx
+++ src/pages/AdminPanel/AdminPartners/EditPartner/EditPartner.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import AdminHeader from "../../../../components/AdminPanel/Header/AdminHeader";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import AdminHeader from "../../../../components/AdminPanel/Header/AdminHeader";
 import AdminInput from "../../../../components/AdminPanel/Input/AdminInput";
 import ImageInput from "../../../../components/AdminPanel/ImageInput/ImageInput";
 import AdminButton from "../../../../components/AdminPanel/UI/Button/AdminButton";
 
 import style from "./EditPartner.module.css";
-
 
 const EditPartner = () => {
   const navigate = useNavigate();
@@ -15,22 +14,53 @@ const EditPartner = () => {
     navigate("/admin/partners");
   };
 
-  const handleImageChange = (newPreview) => {
-    console.log("New Preview:", newPreview);
+  const [inputValue, setInputValue] = useState("");
+  const inputChange = (event) => {
+    setInputValue(event.target.value);
   };
+
+  const [imageValue, setImageValue] = useState("");
+  const imageChange = (newPreview) => {
+    setImageValue(newPreview);
+  };
+
+  const submitFunc = (event) => {
+    event.preventDefault();
+    console.log(imageValue);
+    console.log(inputValue);
+  };
+
   return (
     <div className={style.page}>
-      <AdminHeader withClose closeFunc={closeFunc} heading="Редагувати партнера" />
-      <div className={style.page__content}>
+      <AdminHeader
+        withClose
+        closeFunc={closeFunc}
+        heading="Редагувати партнера"
+      />
+      <form onSubmit={submitFunc} className={style.page__content}>
         <div className={style.inputs}>
-        <AdminInput variant="admin" placeholder="Назва"/>
-        <ImageInput src="https://picsum.photos/200/100" onChange={handleImageChange} variant="project"/>
+          <AdminInput
+            value={inputValue}
+            onChange={inputChange}
+            variant="admin"
+            placeholder="Назва"
+          />
+          <ImageInput
+            src="https://picsum.photos/200/100"
+            value={imageValue}
+            onChange={imageChange}
+            variant="project"
+          />
         </div>
         <div className={style.buttons}>
-          <AdminButton style={{"width": "196px"}} variant="secondary">Скасувати</AdminButton>
-          <AdminButton style={{"width": "196px"}} variant="primary">Зберегти</AdminButton>
+          <AdminButton style={{ width: "196px" }} variant="secondary">
+            Скасувати
+          </AdminButton>
+          <AdminButton type="submit" style={{ width: "196px" }} variant="primary">
+            Зберегти
+          </AdminButton>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
