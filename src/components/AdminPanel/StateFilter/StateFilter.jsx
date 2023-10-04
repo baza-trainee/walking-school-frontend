2c03ef4354f8d5acfc289965d0000db4c3f0eb9a
dsdyaz <dsdy06@gmail.com>
2023-10-05 01:56:26 +0300

    feat: make a state filter component

--- src/components/AdminPanel/StateFilter/StateFilter.jsx
+++ src/components/AdminPanel/StateFilter/StateFilter.jsx
@@ -1,26 +1,55 @@
 import React from "react";
+import Select from "react-select";
+import { ReactComponent as MainIcon } from "../../../assets/images/filterIcon.svg";
 
 import style from "./StateFilter.module.css";
 
-const StateFilter = ({ setFilter }) => {
+const options = [
+  { value: "all", label: "Всі" },
+  { value: "Активний", label: "Активний" },
+  { value: "Неактивний", label: "Неактивний" },
+];
+
+const customStyles = {
+  indicatorSeparator: () => ({
+    display: "none",
+  }),
+};
+
+const StateFilter = ({ currentOption, handleChange, placeholder }) => {
   return (
-    <div className={style.filter}>
-      <select
-        data-testid = "filter"
-        name="stateFilter"
-        onChange={(e) => setFilter(e.target.value)}
-        className={style.filter__select}
-      >
-        <option value="all" className={style.filter__option}>
-          Всі
-        </option>
-        <option value="Активний" className={style.filter__option}>
-          Активний
-        </option>
-        <option value="Неактивний" className={style.filter__option}>
-          Неактивний
-        </option>
-      </select>
+    <div className={style.wrapper}>
+      <Select
+        value={currentOption}
+        placeholder={placeholder}
+        defaultValue={currentOption}
+        onChange={handleChange}
+        options={options}
+        styles={customStyles}
+        menuIsOpen={true}
+        className={style.filter}
+        classNames={{
+          control: (state) => (
+            state.isFocused ? `${style.filter__select} ${style.filter__select_focused}` : `${style.filter__select}`
+          ),
+          menu: () => style.filter__menu,
+          menuList: () => style.filter__menuList,
+          option: () => style.filter__option,
+          placeholder: () => style.filter__placeholder,
+          singleValue: () => style.filter__single_value,
+        }}
+        components={{
+          DropdownIndicator: IconClosed,
+        }}
+      />
+    </div>
+  );
+};
+
+const IconClosed = ({ innerRef, innerProps }) => {
+  return (
+    <div ref={innerRef} {...innerProps}>
+      <MainIcon />
     </div>
   );
 };
