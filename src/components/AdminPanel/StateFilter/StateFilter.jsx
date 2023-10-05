71361d20f36dd3157f8f5a45134597b9abf1c1a4
dsdyaz <dsdy06@gmail.com>
2023-10-05 03:35:00 +0300

    feat: finish stateFilter component

--- src/components/AdminPanel/StateFilter/StateFilter.jsx
+++ src/components/AdminPanel/StateFilter/StateFilter.jsx
@@ -14,44 +14,51 @@ const customStyles = {
   indicatorSeparator: () => ({
     display: "none",
   }),
+  dropdownIndicator: () => ({
+    display: "none",
+  }),
 };
 
 const StateFilter = ({ currentOption, handleChange, placeholder }) => {
   return (
-    <div className={style.wrapper}>
-      <Select
-        value={currentOption}
-        placeholder={placeholder}
-        defaultValue={currentOption}
-        onChange={handleChange}
-        options={options}
-        styles={customStyles}
-        menuIsOpen={true}
-        className={style.filter}
-        classNames={{
-          control: (state) => (
-            state.isFocused ? `${style.filter__select} ${style.filter__select_focused}` : `${style.filter__select}`
-          ),
-          menu: () => style.filter__menu,
-          menuList: () => style.filter__menuList,
-          option: () => style.filter__option,
-          placeholder: () => style.filter__placeholder,
-          singleValue: () => style.filter__single_value,
-        }}
-        components={{
-          DropdownIndicator: IconClosed,
-        }}
-      />
-    </div>
+    <Select
+      openMenuOnClick={true}
+      closeMenuOnSelect={true}
+      value={currentOption}
+      placeholder={placeholder}
+      defaultValue={currentOption}
+      onChange={handleChange}
+      options={options}
+      styles={customStyles}
+      unstyled={true}
+      className={style.filter}
+      classNames={{
+        control: (state) =>
+          state.menuIsOpen
+            ? `${style.filter__select} ${style.filter__select_focused}`
+            : `${style.filter__select}`,
+        menu: () => style.filter__menu,
+        menuList: () => style.filter__menuList,
+        option: () => style.filter__option,
+        placeholder: () => style.filter__placeholder,
+        singleValue: () => style.filter__single_value,
+        valueContainer: () => style.filter__valueContainer,
+        input: () => style.filter__input,
+        indicatorsContainer: () => style.filter__dropdownIcon,
+      }}
+      // components={{
+      //   DropdownIndicator: IconClosed,
+      // }}
+    />
   );
 };
 
-const IconClosed = ({ innerRef, innerProps }) => {
-  return (
-    <div ref={innerRef} {...innerProps}>
-      <MainIcon />
-    </div>
-  );
-};
+// const IconClosed = ({ innerRef, innerProps }) => {
+//   return (
+//     <div className={style.iconWrapper} ref={innerRef} {...innerProps}>
+//       <MainIcon />
+//     </div>
+//   );
+// };
 
 export default StateFilter;
