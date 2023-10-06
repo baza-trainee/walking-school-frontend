31b8a5c7f9cb75a5747934a2171f69681d827a5e
Kateryna Lanina <124586601+lanina90@users.noreply.github.com>
2023-10-06 14:36:43 +0300

    hotfix: added Container to Projects; add borderradius to hover link in header according design (#137)

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import ProjectCard from "./ProjectCard/ProjectCard";
 import { useMedia } from "../../hooks/useMedia";
 import Button from "../UI/Button/Button";
 import SwiperSlider from "./Carousel/SwiperSlider";
+import Container from "../layout/Container";
 const items = [
   {
     image:
@@ -59,49 +60,53 @@ const Projects = () => {
   const cardsToRender = cards;
 
   return (
-    <section className={style.projects} id="projects">
-      <div className={style.projects__title}>
-        <div>
-          <h2>Проєкти</h2>
-        </div>
-        <p>
-          Спробую навчити Вас ходити у незвичних умовах, та всьому що сам знаю.
-          Ми вчитемося ходити у Польських Татрах Спробую навчити
-        </p>
-      </div>
+    <section id="projects">
+      <Container>
+        <div className={style.projects}>
+          <div className={style.projects__title}>
+            <div>
+              <h2>Проєкти</h2>
+            </div>
+            <p>
+              Спробую навчити Вас ходити у незвичних умовах, та всьому що сам
+              знаю. Ми вчитемося ходити у Польських Татрах Спробую навчити
+            </p>
+          </div>
 
-      <div className={style.carousel}>
-        {isMobile ? (
-          <div
-            data-testid={"mobile-slider"}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              gap: "24px",
-            }}
-          >
-            {cardsToRender?.slice(0, 3).map((item, index) => (
-              <Fragment key={index}>
-                <ProjectCard
-                  image={item.image}
-                  title={item.title}
-                  dates={item.dates}
-                  age={item.age}
-                  description={item.description}
-                />
-              </Fragment>
-            ))}
-            {cardsToRender.length > 3 && (
-              <Button variant={"tertiary"} style={{ width: "100%" }}>
-                Дивитись більше
-              </Button>
+          <div className={style.carousel}>
+            {isMobile ? (
+              <div
+                data-testid={"mobile-slider"}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: "24px",
+                }}
+              >
+                {cardsToRender?.slice(0, 3).map((item, index) => (
+                  <Fragment key={index}>
+                    <ProjectCard
+                      image={item.image}
+                      title={item.title}
+                      dates={item.dates}
+                      age={item.age}
+                      description={item.description}
+                    />
+                  </Fragment>
+                ))}
+                {cardsToRender.length > 3 && (
+                  <Button variant={"tertiary"} style={{ width: "100%" }}>
+                    Дивитись більше
+                  </Button>
+                )}
+              </div>
+            ) : (
+              <SwiperSlider items={cardsToRender} />
             )}
           </div>
-        ) : (
-          <SwiperSlider items={cardsToRender} />
-        )}
-      </div>
+        </div>
+      </Container>
     </section>
   );
 };
