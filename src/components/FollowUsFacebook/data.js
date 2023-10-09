8bef27bebec0a4e19cec5ac32e554c0482a3ae09
dsdyaz <dsdy06@gmail.com>
2023-10-09 18:41:01 +0300

    fix: move assets of followUs module

--- src/components/FollowUsFacebook/data.js
+++ src/components/FollowUsFacebook/data.js
@@ -1,7 +1,7 @@
-import facebook1 from "../../assets/followUs/download.jpg";
-import facebook2 from "../../assets/followUs/img.webp";
-import facebook3 from "../../assets/followUs/facebook3.png";
-import facebook4 from "../../assets/followUs/facebook4.png";
+import facebook1 from "../../assets/main/followUs/facebook1.png";
+import facebook2 from "../../assets/main/followUs/img.webp";
+import facebook3 from "../../assets/main/followUs/facebook3.png";
+import facebook4 from "../../assets/main/followUs/facebook4.png";
 
 export const data = [
   {
