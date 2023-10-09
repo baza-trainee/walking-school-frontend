22e33a7a3e6a5e7b7ea2e7227036906c5a08a23b
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-10-09 10:35:25 +0300

    fix: fix styles to Hero, body:overflow:hidden

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -7,7 +7,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination, Autoplay } from "swiper/modules";
 
-const HeroSection = () => {
+const Hero = () => {
   const { isTablet, isDesktop } = useMedia();
   let data = [];
   if (isDesktop) {
@@ -45,4 +45,4 @@ const HeroSection = () => {
   );
 };
 
-export default HeroSection;
+export default Hero;
