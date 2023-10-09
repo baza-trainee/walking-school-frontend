ac2184286b6fe2b9b470914889cc9b4642482810
Kateryna Laina <boba4ok@gmail.com>
2023-10-09 12:20:07 +0300

    commit

--- src/components/layout/Main/Main.jsx
+++ src/components/layout/Main/Main.jsx
@@ -1,15 +1,18 @@
+import React, { Suspense, lazy } from "react";
 import { Scroller } from "../../Scroller/Scroller";
 import { Offerings } from "../../Offerings/Offerings";
-import Projects from "../../Projects/Projects";
 import { Contact } from "../../Contact/Contact";
 import Requirements from "../../Requirements/Requirements";
-import Participate from "../../Participate/Participate";
+// import Participate from "../../Participate/Participate";
 import FollowUsFacebook from "../../FollowUsFacebook/index.jsx";
 import HeroSection from "../../HeroSection/index";
 import Adventures from "../../Adventures/Adventures";
 import { Partners } from "../../Partners/Partners";
 import ScrollUp from "../../ScrollUp/ScrollUp";
 import styles from "./Main.module.css";
+
+const Projects = lazy(() => import("../../Projects/Projects"));
+const Participate = lazy(() => import("../../Participate/Participate"));
 
 const Main = () => (
   // eslint-disable-next-line react/prop-types
