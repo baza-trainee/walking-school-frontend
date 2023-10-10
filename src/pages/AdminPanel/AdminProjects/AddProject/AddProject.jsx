49c878b5252a8cc4b442f7172425724bab43361e
Denys Topchyi <90519083+Blenemy@users.noreply.github.com>
2023-10-10 18:58:46 +0300

    Feature/issue 73/projects admin component (#157)
    * feat: add basic implementation
    
    * feat: finished projects section
    
    * feat: add basic logic to add a project component
    
    * fix: fix bugs
    
    * fix: improved dateselect, replaced adminheader
    
    * ref: used reusable components
    
    * fix: changed route, fixed some styles
    
    * fix: improved validation for the form
    
    * fix: changed redirection to the right page
    
    * fix: changed name
    
    * fix: improved styles, used reusable filter comp
    
    * fix: fixed svg path in filterDropdown
    
    * ref: used formik in addProject component, improved logic

--- src/pages/AdminPanel/AdminProjects/AddProject/AddProject.jsx
+++ src/pages/AdminPanel/AdminProjects/AddProject/AddProject.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { CustomSelect } from "../../../../components/AdminPanel/Filters/CustomSelect/CustomSelect";
 import { DateSelect } from "../../../../components/AdminPanel/Filters/DateSelect/DateSelect";
 import ImageInput from "../../../../components/AdminPanel/ImageInput/ImageInput";
@@ -7,7 +9,6 @@ import AdminButton from "../../../../components/AdminPanel/UI/Button/AdminButton
 import { Tooltip } from "../Tooltip/Tooltip";
 import styles from "./AddProject.module.css";
 import AdminHeader from "../../../../components/AdminPanel/Header/AdminHeader";
-// import { useParams } from "react-router-dom";
 
 const MockedOptions = [
   "Тунельний політ",
@@ -19,88 +20,76 @@ const MockedOptions = [
 const MockedOptions2 = ["0-18", "18-60"];
 
 export const AddProject = () => {
-  // TODO: use custom hook and move the logic there
-  // TODO: use Formik (probably)
-  // const { id } = useParams();
-  const [formData, setFormData] = useState({
-    title: "",
-    link: "",
-    description: "",
-    publishDate: null,
-    eventDate: null,
-    ageLimit: null,
-    category: null,
-    image: null,
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      link: "",
+      description: "",
+      publishDate: null,
+      eventDate: null,
+      ageLimit: null,
+      category: null,
+      image: null,
+    },
+    validationSchema: Yup.object({
+      title: Yup.string().required("Обов'язкове поле"),
+      link: Yup.string().url("Невірний формат посилання"),
+      description: Yup.string().required("Обязательное поле"),
+      eventDate: Yup.string()
+        .matches(
+          /^(0[1-9]|1[0-2])\.\d{4} - (0[1-9]|1[0-2])\.\d{4}$/,
+          "Невірний формат",
+        )
+        .required("Обов'язкове поле"),
+    }),
+    onSubmit: (values) => {
+      if (!formik.values.publishDate) {
+        const currentDate = new Date();
+        formik.values.publishDate = `${String(
+          currentDate.getMonth() + 1,
+        ).padStart(2, "0")}.${currentDate.getFullYear()}`;
+      }
+
+      console.log("Sending data:", values);
+    },
   });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleImageChange = (blob) => {
-    setFormData({ ...formData, image: blob });
-  };
-
-  const handleDateChange = (field, date) => {
-    if (!date) {
-      return;
-    }
-    setFormData({ ...formData, [field]: date });
-  };
-
-  const handleSelectChange = (fieldName) => (selectedOption) => {
-    setFormData({ ...formData, [fieldName]: selectedOption });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const date = new Date();
-    const currentDate = `${date.getFullYear()}-${
-      date.getMonth() + 1
-    }-${date.getDate()}`;
-
-    const finalFormData = {
-      ...formData,
-      publishDate: formData.publishDate ? formData.publishDate : currentDate,
-    };
-
-    console.log("Sending data:", finalFormData);
-  };
-
-  useEffect(() => {
-    // TODO: use the id param to fetch needed project and set form fields accordingly
-  }, []);
 
   return (
     <>
       <AdminHeader heading={"Додати проєкт"} withClose={true} />
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <div className={styles.content}>
           <div className={styles.leftBlock}>
             <AdminInput
               name="title"
               variant="admin"
               placeholder="Заголовок"
-              onChange={handleInputChange}
+              onChange={formik.handleChange}
+              value={formik.values.title}
+              error={formik.touched.title && formik.errors.title}
             />
             <AdminInput
               name="link"
               variant="admin"
               placeholder="Додайте посилання"
-              onChange={handleInputChange}
+              onChange={formik.handleChange}
+              value={formik.values.link}
+              error={formik.touched.link && formik.errors.link}
             />
             <AdminInput
               name="description"
               variant="textarea"
               placeholder="Опис"
-              onChange={handleInputChange}
+              onChange={formik.handleChange}
+              value={formik.values.description}
+              error={formik.touched.description && formik.errors.description}
             />
             <div className={styles.tooltipContainer}>
               <DateSelect
                 placeholder={"Дата публікації"}
-                onChange={(date) => handleDateChange("publishDate", date)}
+                onChange={(date) => formik.setFieldValue("publishDate", date)}
                 id={"publishDate"}
+                error={formik.touched.publishDate && formik.errors.publishDate}
               />
               <Tooltip />
             </div>
@@ -116,24 +105,27 @@ export const AddProject = () => {
           <div className={styles.rightBlock}>
             <DateSelect
               placeholder={"Період"}
-              onChange={(date) => handleDateChange("eventDate", date)}
+              onChange={(date) => formik.setFieldValue("eventDate", date)}
               id={"eventDate"}
+              error={formik.touched.eventDate && formik.errors.eventDate}
             />
             <CustomSelect
               options={MockedOptions}
-              onChange={handleSelectChange("category")}
+              onChange={(option) => formik.setFieldValue("category", option)}
+              error={formik.touched.category && formik.errors.category}
             />
             <CustomSelect
               options={MockedOptions2}
-              onChange={handleSelectChange("ageLimit")}
+              onChange={(option) => formik.setFieldValue("ageLimit", option)}
               placeholder="Вікові обмеження"
               selectPrompt="Оберіть вік"
+              error={formik.touched.ageLimit && formik.errors.ageLimit}
             />
             <ImageInput
               variant="project"
-              onChange={handleImageChange}
+              onChange={(blob) => formik.setFieldValue("image", blob)}
               src={null}
-              error={null}
+              error={formik.touched.image && formik.errors.image}
             />
           </div>
         </div>
