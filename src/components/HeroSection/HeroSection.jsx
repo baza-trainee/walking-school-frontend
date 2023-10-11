b03391ba85b53c7a31c37ed5d84b3eeb23262308
dsdyaz <dsdy06@gmail.com>
2023-10-12 00:05:01 +0300

    feat: make base for hero component

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,48 @@
+import { dataMob, dataDesc, dataTab } from "./data";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useMedia } from "../../hooks/useMedia";
+import { Slide } from "./Slide";
+import "./pagination.css";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
+
+const HeroSection = (data) => {
+  // const { isTablet, isDesktop } = useMedia();
+  // let data = [];
+  // if (isDesktop) {
+  //   data = dataDesc;
+  // } else if (isTablet) {
+  //   data = dataTab;
+  // } else {
+  //   data = dataMob;
+  // }
+
+  return (
+    <section data-testid="hero-slider">
+      <Swiper
+        slidesPerView={1}
+        autoplay={{
+          delay: 7000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination, Autoplay]}
+      >
+        {data.map((element, index) => (
+          <SwiperSlide key={index} role="slide" className="swiper-slide">
+            <Slide
+              img={element.img}
+              title={element.title}
+              description={element.description}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,48 +1,23 @@
-import { dataMob, dataDesc, dataTab } from "./data";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { useMedia } from "../../hooks/useMedia";
-import { Slide } from "./Slide";
-import "./pagination.css";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Pagination, Autoplay } from "swiper/modules";
+import React from "react";
+import HeroSection from "./HeroSection";
+import { useQuery } from "@tanstack/react-query";
+import { getHero } from "../../API/hero";
 
-const HeroSection = () => {
-  const { isTablet, isDesktop } = useMedia();
-  let data = [];
-  if (isDesktop) {
-    data = dataDesc;
-  } else if (isTablet) {
-    data = dataTab;
-  } else {
-    data = dataMob;
+const Hero = () => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["hero"],
+    queryFn: getHero,
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
-  return (
-    <section data-testid="hero-slider">
-      <Swiper
-        slidesPerView={1}
-        autoplay={{
-          delay: 7000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination, Autoplay]}
-      >
-        {data.map((element, index) => (
-          <SwiperSlide key={index} role="slide" className="swiper-slide">
-            <Slide
-              img={element.img}
-              title={element.title}
-              description={element.description}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </section>
-  );
+  if (!!error) {
+    return <div>{error.message}</div>;
+  }
+
+  return <HeroSection data={data} />;
 };
 
-export default HeroSection;
+export default Hero;
