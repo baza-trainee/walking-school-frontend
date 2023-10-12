cde36dbddc567201a9cea9ace6fc739c9b978fbc
dsdyaz <dsdy06@gmail.com>
2023-10-12 22:02:27 +0300

    feat: add fetching and deleting func to adminHero

--- src/pages/AdminPanel/HeroAdmin/MainHero.jsx
+++ src/pages/AdminPanel/HeroAdmin/MainHero.jsx
@@ -5,34 +5,51 @@ import AdminHeader from "../../../components/AdminPanel/Header/AdminHeader";
 import { useNavigate } from "react-router-dom";
 import HeroBlock from "../../../components/AdminPanel/Hero/HeroBlock/HeroBlock";
 import Alert from "../../../components/AdminPanel/Alert/Alert";
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
+import { getHero, deleteHero } from "../../../API/hero";
 
 const MainHero = () => {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
+  const [currentId, setCurrentId] = useState("");
 
-  const items = [
-    {
-      id: 1,
-      img: "https://images.unsplash.com/photo-1696509528129-c28dc0308733?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      title: "Школа ходи",
-      subtitle:
-        "Наша Школа ходи для ветеранів - це не просто набір екстремальних пригод. Це потужний інструмент для відновлення та підвищення якості життя.",
-    },
-    {
-      id: 2,
-      img: "https://images.unsplash.com/photo-1696509528129-c28dc0308733?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      title: "Школа ходи",
-      subtitle:
-        "Наша Школа ходи для ветеранів - це не просто набір екстремальних пригод. Це потужний інструмент для відновлення та підвищення якості життя.",
-    },
-    {
-      id: 3,
-      img: "https://images.unsplash.com/photo-1696509528129-c28dc0308733?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
-      title: "Школа ходи",
-      subtitle:
-        "Наша Школа ходи для ветеранів - це не просто набір екстремальних пригод. Це потужний інструмент для відновлення та підвищення якості життя.",
-    },
-  ];
+  // const data = [
+  //   {
+  //     id: 1,
+  //     img: "https://images.unsplash.com/photo-1696509528129-c28dc0308733?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+  //     title: "Школа ходи",
+  //     subtitle:
+  //       "Наша Школа ходи для ветеранів - це не просто набір екстремальних пригод. Це потужний інструмент для відновлення та підвищення якості життя.",
+  //   },
+  //   {
+  //     id: 2,
+  //     img: "https://images.unsplash.com/photo-1696509528129-c28dc0308733?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+  //     title: "Школа ходи",
+  //     subtitle:
+  //       "Наша Школа ходи для ветеранів - це не просто набір екстремальних пригод. Це потужний інструмент для відновлення та підвищення якості життя.",
+  //   },
+  //   {
+  //     id: 3,
+  //     img: "https://images.unsplash.com/photo-1696509528129-c28dc0308733?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+  //     title: "Школа ходи",
+  //     subtitle:
+  //       "Наша Школа ходи для ветеранів - це не просто набір екстремальних пригод. Це потужний інструмент для відновлення та підвищення якості життя.",
+  //   },
+  // ];
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["hero"],
+    queryFn: getHero,
+  });
+
+  const mutation = useMutation(deleteHero, {
+    onSuccess: () => QueryClient.invalidateQueries(["hero"]),
+  });
+
+  const deleteFunc = (id) => {
+    console.log(id)
+    mutation.mutate(id);
+  };
   return (
     <div>
       <AdminHeader
@@ -41,14 +58,17 @@ const MainHero = () => {
         buttonFunc={() => navigate("add")}
       />
       <div className={styles.container}>
-        {items ? (
-          items.map((item) => (
+        {data ? (
+          data.map((item) => (
             <React.Fragment key={item.id}>
               <HeroBlock
                 image={item.img}
                 title={item.title}
                 subtitle={item.subtitle}
-                removeHandler={() => setIsOpen(true)}
+                removeHandler={() => {
+                  setIsOpen(true);
+                  setCurrentId(item.id);
+                }}
                 editHandler={() => navigate(`edit/${item.id}`)}
               />
             </React.Fragment>
@@ -62,7 +82,7 @@ const MainHero = () => {
             setActive={setIsOpen}
             title={"Видалити слайд"}
             message={"Ви точно хочете видалити слайд?"}
-            successFnc={() => console.log("deleted")}
+            successFnc={() => deleteFunc(currentId)}
           />
         )}
       </div>
