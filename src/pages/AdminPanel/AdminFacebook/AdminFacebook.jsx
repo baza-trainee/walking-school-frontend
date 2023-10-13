efe10512bad12956b5bbfaed9991124f772459f6
dsdyaz <dsdy06@gmail.com>
2023-10-13 22:58:33 +0300

    feat: add query and mutation to AdminFacebook

--- src/pages/AdminPanel/AdminFacebook/AdminFacebook.jsx
+++ src/pages/AdminPanel/AdminFacebook/AdminFacebook.jsx
@@ -2,32 +2,75 @@ import React, { useState } from "react";
 import AdminHeader from "../../../components/AdminPanel/Header/AdminHeader";
 import ImageInput from "../../../components/AdminPanel/ImageInput/ImageInput";
 import AdminButton from "../../../components/AdminPanel/UI/Button/AdminButton";
-
+import { blobUrlToBase64 } from "../../../heplers/BlobToBase64";
 import style from "./AdminFacebook.module.css";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getFacebook, putFacebook } from "../../../API/followUsFacebook";
 
-const defaultValues = {
-  0: "",
-  1: "",
-  2: "",
-  3: "",
-  4: "",
-  5: "",
-};
+const defaultValues = [
+  { id: 1, image: "" },
+  { id: 2, image: "" },
+  { id: 3, image: "" },
+  { id: 4, image: "" },
+  { id: 5, image: "" },
+  { id: 6, image: "" },
+];
 
 const AdminFacebook = () => {
   const [values, setValues] = useState(defaultValues);
 
   const handleImageChange = (index, newPreview) => {
     setValues((prevValues) => {
-      const updatedValues = { ...prevValues };
-      updatedValues[index] = newPreview || "";
+      const updatedValues = [...prevValues];
+      updatedValues[index - 1] = {
+        ...updatedValues[index - 1],
+        image: newPreview || "",
+      };
       return updatedValues;
     });
   };
 
-  const submitFunc = (event) => {
+  const { data, loading, error } = useQuery({
+    queryKey: ["facebook"],
+    queryFn: getFacebook,
+  });
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: putFacebook,
+    onSettled: () => queryClient.invalidateQueries(["facebook"]),
+  });
+
+  async function transformValues(values) {
+    const transformed = await Promise.all(
+      values.map(async (value) => {
+        if (value.image) {
+          const image = await blobUrlToBase64(value.image);
+          return {
+            id: value.id,
+            image: image,
+          };
+        } else {
+          return {
+            id: value.id,
+            image: null,
+          };
+        }
+      })
+    );
+    return transformed;
+  }
+
+  const submitFunc = async (event) => {
     event.preventDefault();
-    console.log(values);
+    const transformedValues = await transformValues(values)
+    try{
+      await mutation.mutateAsync(transformedValues)
+    }
+    catch(error) {
+      console.log(error)
+    }
   };
 
   return (
@@ -36,15 +79,15 @@ const AdminFacebook = () => {
       <div className={style.content}>
         <form onSubmit={submitFunc} className={style.form}>
           <div className={style.form__inputs}>
-            {Object.keys(defaultValues).map((index) => (
+            {values.map((element) => (
               <ImageInput
-                key={index}
-                value={values[parseInt(index)]}
+                key={element.id}
+                value={values[element]}
                 onChange={(newPreview) =>
-                  handleImageChange(parseInt(index), newPreview)
+                  handleImageChange(element.id, newPreview)
                 }
                 variant="facebook"
-                name={index}
+                name={element.id}
               />
             ))}
           </div>
