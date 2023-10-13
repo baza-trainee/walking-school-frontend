454e1e12b9459bb59477b89468f2612fa2a74de3
Denys Topchyi <miliarder97@gmail.com>
2023-10-13 19:42:27 +0300

    feat: add request on projectForm submittion and get all projects

--- src/pages/AdminPanel/AdminProjects/AddProject/AddProject.jsx
+++ src/pages/AdminPanel/AdminProjects/AddProject/AddProject.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-import { useFormik } from "formik";
-import * as Yup from "yup";
 import { CustomSelect } from "../../../../components/AdminPanel/Filters/CustomSelect/CustomSelect";
 import { DateSelect } from "../../../../components/AdminPanel/Filters/DateSelect/DateSelect";
 import ImageInput from "../../../../components/AdminPanel/ImageInput/ImageInput";
@@ -9,50 +6,13 @@ import AdminButton from "../../../../components/AdminPanel/UI/Button/AdminButton
 import { Tooltip } from "../Tooltip/Tooltip";
 import styles from "./AddProject.module.css";
 import AdminHeader from "../../../../components/AdminPanel/Header/AdminHeader";
-
-const MockedOptions = [
-  "Тунельний політ",
-  "Банджі джампінг",
-  "Стрибки з парашутом",
-  "Стежка у хмарах",
-];
-
-const MockedOptions2 = ["0-18", "18-60"];
+import { ageOptions, eventOptions } from "../optionsData";
+import { useParams } from "react-router";
+import { useProjectForm } from "../../../../hooks/useProjectForm";
 
 export const AddProject = () => {
-  const formik = useFormik({
-    initialValues: {
-      title: "",
-      link: "",
-      description: "",
-      publishDate: null,
-      eventDate: null,
-      ageLimit: null,
-      category: null,
-      image: null,
-    },
-    validationSchema: Yup.object({
-      title: Yup.string().required("Обов'язкове поле"),
-      link: Yup.string().url("Невірний формат посилання"),
-      description: Yup.string().required("Обязательное поле"),
-      eventDate: Yup.string()
-        .matches(
-          /^(0[1-9]|1[0-2])\.\d{4} - (0[1-9]|1[0-2])\.\d{4}$/,
-          "Невірний формат",
-        )
-        .required("Обов'язкове поле"),
-    }),
-    onSubmit: (values) => {
-      if (!formik.values.publishDate) {
-        const currentDate = new Date();
-        formik.values.publishDate = `${String(
-          currentDate.getMonth() + 1,
-        ).padStart(2, "0")}.${currentDate.getFullYear()}`;
-      }
-
-      console.log("Sending data:", values);
-    },
-  });
+  const { id } = useParams();
+  const { formik } = useProjectForm(id);
 
   return (
     <>
@@ -110,12 +70,12 @@ export const AddProject = () => {
               error={formik.touched.eventDate && formik.errors.eventDate}
             />
             <CustomSelect
-              options={MockedOptions}
+              options={eventOptions}
               onChange={(option) => formik.setFieldValue("category", option)}
               error={formik.touched.category && formik.errors.category}
             />
             <CustomSelect
-              options={MockedOptions2}
+              options={ageOptions}
               onChange={(option) => formik.setFieldValue("ageLimit", option)}
               placeholder="Вікові обмеження"
               selectPrompt="Оберіть вік"
--- src/pages/AdminPanel/AdminProjects/AdminProjects.jsx
+++ src/pages/AdminPanel/AdminProjects/AdminProjects.jsx
@@ -2,56 +2,11 @@ import { useState } from "react";
 import { ProjectsList } from "./ProjectsList/ProjectsList";
 import AdminHeader from "../../../components/AdminPanel/Header/AdminHeader";
 import { useNavigate } from "react-router-dom";
-
-// dummy data for testing purposes
-const data = [
-  {
-    id: 1,
-    creation_date: "10.11.2022",
-    status: "Активний",
-    project_name: "Банджі джампінг",
-  },
-  {
-    id: 2,
-    creation_date: "01.09.2023",
-    status: "Неактивний",
-    project_name: "Кафе на даху",
-  },
-  {
-    id: 3,
-    creation_date: "15.06.2022",
-    status: "Активний",
-    project_name: "Доставка їжі на дронах",
-  },
-  {
-    id: 4,
-    creation_date: "22.03.2022",
-    status: "Активний",
-    project_name: "Віртуальні екскурсії",
-  },
-  {
-    id: 5,
-    creation_date: "30.08.2022",
-    status: "Неактивний",
-    project_name: "Магазин еко-продуктів",
-  },
-  {
-    id: 6,
-    creation_date: "18.07.2023",
-    status: "Активний",
-    project_name: "Сервіс каршерингу",
-  },
-  {
-    id: 7,
-    creation_date: "05.04.2021",
-    status: "Неактивний",
-    project_name: "Онлайн кінотеатр",
-  },
-];
+import { useGetAdminProjects } from "../../../hooks/useGetAdminProjects";
 
 export const AdminProjects = () => {
+  const { setProjectsData, projectsData, isLoading } = useGetAdminProjects();
   const [searchTerm, setSearchTerm] = useState("");
-  const [projectsData, setProjectsData] = useState(data);
   const [sortDirection, setSortDirection] = useState("asc");
   const navigate = useNavigate();
 
@@ -82,9 +37,10 @@ export const AdminProjects = () => {
     // TODO: implement logic for deleting a project using projectId and backend endpoint
   };
 
-  // TODO: use useEffect or useQuery with endpoints to get the projects from the server
-  // TODO: use state to put the data into it.
-  // TODO: replace dummy data with the state
+  if (isLoading) {
+    return <div>Loading....</div>;
+  }
+
   return (
     <>
       <AdminHeader
