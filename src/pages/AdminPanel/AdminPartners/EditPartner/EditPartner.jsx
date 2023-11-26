59f7834e9a13cdf9e85785a212a89cd89ef0b335
dsdyaz <102921963+dsdyaz@users.noreply.github.com>
2023-11-26 23:16:35 +0200

    Feat/card#93/http to partners2 (#236)
    * fix: change res.code check
    
    * feat: add partners rendering to the page
    
    * refactor: rearrange imports
    
    * feat: add deps to AdminPartners useEffect
    
    * feat: update partner editing
    
    * fix: fix error in image input props
    
    * feat: add centered style for edit partners
    
    * feat: update EditPartner
    
    * feat: add centered style to AdminPartners
    
    * fix: fix deletion in AdminPartners
    
    * feat: update sorting function
    
    * feat: add leaving modals to the EditPartner
    
    * refactor: remove unused func
    
    * refactor: format EditPartner
    
    * feat: add modals and loaders to AddPartner
    
    * refactor: rearrange renders
    
    * refactor: delete unused statement
    
    * fix: change variant of inputs to Partner
    
    * feat: add partners loading to main
    
    * feat: add slides rendering
    
    * chore: update sliders css
    
    * fix: add missing success toggle
    
    * fix: try to fix an error with filter is not func
    
    * try to fix issue with preview not being added
    
    * feat: add deletion modal
    
    * fix: change message in modal
    
    * fix: add rerender after partner updates
    
    * new try
    
    * new try
    
    * fix: fix input element error
    
    * try
    
    * fix: fix unwanted merge changes

--- src/pages/AdminPanel/AdminPartners/EditPartner/EditPartner.jsx
+++ src/pages/AdminPanel/AdminPartners/EditPartner/EditPartner.jsx
@@ -146,7 +146,7 @@ const EditPartner = () => {
           />
           <ImageInput
             src={partner.image ? partner.image : ""}
-            value={partner.image ? partner.image : ""}
+            value={""}
             onChange={(newPreview) => imageChange(newPreview)}
             handleClear={() => handleDelete}
             variant="partner"
