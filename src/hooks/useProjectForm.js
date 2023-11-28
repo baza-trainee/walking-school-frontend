101573f3cab73139823b6fb6075929641516ba23
Denys Topchyi <miliarder97@gmail.com>
2023-11-28 16:48:09 +0200

    feat: add validation to project fields

--- src/hooks/useProjectForm.js
+++ src/hooks/useProjectForm.js
@@ -71,8 +71,6 @@ export const useProjectForm = (projectId, project) => {
           valuesToSend.last_modified = `${currentMonth}-${currentYear}`;
         }
 
-        console.log(valuesToSend);
-
         mutation.mutate(valuesToSend);
       } catch (error) {
         setLocalError(error);
