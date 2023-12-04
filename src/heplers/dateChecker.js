852638de85d2c1cd09774d57f6d6bd19f7dff24f
Denys Topchyi <miliarder97@gmail.com>
2023-12-04 15:36:01 +0200

    feat: improved logic to check current date

--- src/heplers/dateChecker.js
+++ src/heplers/dateChecker.js
@@ -5,9 +5,20 @@ export const dateChecker = (date) => {
 
   const currentDate = new Date();
   const currentMonth = currentDate.getMonth() + 1;
+  const currentYear = currentDate.getFullYear();
 
   const endDateString = date.split(" - ")[1];
-  const [endMonth] = endDateString.split(".");
+  const [endMonth, endYear] = endDateString.split(".");
 
-  return currentMonth <= parseInt(endMonth, 10);
+  const endMonthNum = parseInt(endMonth, 10);
+  const endYearNum = parseInt(endYear, 10);
+
+  if (
+    currentYear < endYearNum ||
+    (currentYear === endYearNum && currentMonth <= endMonthNum)
+  ) {
+    return true;
+  }
+
+  return false;
 };
