44df7144b09030560215f2431c29ac581d38c91f
Denys Topchyi <miliarder97@gmail.com>
2023-12-04 15:37:38 +0200

    ref: include jsdoc description to datechecker function

--- src/heplers/dateChecker.js
+++ src/heplers/dateChecker.js
@@ -1,3 +1,16 @@
+/**
+ * Checks whether the current date is less than or equal to the project's end date.
+ * The end date of the project is provided in the format "MM.YYYY - MM.YYYY".
+ *
+ * If the provided date string is empty or null, the function returns true,
+ * implying that there is no restriction based on the end date.
+ *
+ * @param {string} date - The date string representing the start and end dates of the project, separated by " - ".
+ *                        For example, "11.2023 - 12.2023".
+ * @returns {boolean} - Returns true if the current date is before or in the same month and year as the end date;
+ *                      returns false otherwise.
+ */
+
 export const dateChecker = (date) => {
   if (!date) {
     return true;
