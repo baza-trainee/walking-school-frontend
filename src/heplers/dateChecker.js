3e34df7e9dbb78f3cf5d4e43946ff839e4d07206
Denys Topchyi <90519083+Blenemy@users.noreply.github.com>
2023-12-04 15:47:33 +0200

    Feature/issue 73/projects admin component (#263)
    * feat: add basic implementation
    
    * feat: finished projects section
    
    * feat: add basic logic to add a project component
    
    * fix: fix bugs
    
    * fix: improved dateselect, replaced adminheader
    
    * ref: used reusable components
    
    * fix: changed route, fixed some styles
    
    * fix: improved validation for the form
    
    * fix: changed redirection to the right page
    
    * fix: changed name
    
    * fix: improved styles, used reusable filter comp
    
    * fix: fixed svg path in filterDropdown
    
    * ref: used formik in addProject component, improved logic
    
    * feat: add request on projectForm submittion and get all projects
    
    * feat: add delete project functionality
    
    * bug: fix customSelect range and error messages
    
    * fix: changed date select, add loading state to buttons, add redirect func on cancel btn
    
    * fix: fix url form request
    
    * feat: changed loader for admin projecets
    
    * fix: changed date format that is send via req
    
    * feat: add basic logic to delete project func
    
    * feat: add message alert on req fail
    
    * ref: refactored alert state
    
    * feat: enhance buttons in addProject
    
    * feat: add update description functionality
    
    * ref: delete dummy data
    
    * ref: adjusted filter to backend data
    
    * fix: delete unnecessary feature
    
    * fix: adjusted project page to backend data
    
    * fix: adjusted to backend data
    
    * fix: adjust admin project section to backend data
    
    * ref: fix router version
    
    * fix: add id to put request
    
    * fix: add default value to edit input
    
    * fix: fix bugs with date and empty project stack
    
    * fix: convert image to blob format
    
    * fix: fix bugs with patched data on request
    
    * fix: fix bugs with invalid date
    
    * ref: delete console log
    
    * feat: add date validation
    
    * feat: moved schema to separate folder
    
    * feat: add validation to project fields
    
    * fix: fix validating project status via date
    
    * ref: delete console
    
    * feat: improved logic to check current date
    
    * ref: include jsdoc description to datechecker function
    
    * ref: refactored useProjectForm custom hook
    
    ---------
    
    Co-authored-by: Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>

--- src/heplers/dateChecker.js
+++ src/heplers/dateChecker.js
@@ -1,3 +1,16 @@
+/**
+ * Checks whether the current date is less than or equal to the project's end date.
+ * The end date of the project is provided in the format "MM.YYYY - MM.YYYY".
+ *
+ * If the provided date string is empty or null, the function returns true,
+ * implying that there is no restriction based on the end date.
+ *
+ * @param {string} date - The date string representing the start and end dates of the project, separated by " - ".
+ *                        For example, "11.2023 - 12.2023".
+ * @returns {boolean} - Returns true if the current date is before or in the same month and year as the end date;
+ *                      returns false otherwise.
+ */
+
 export const dateChecker = (date) => {
   if (!date) {
     return true;
@@ -5,9 +18,20 @@ export const dateChecker = (date) => {
 
   const currentDate = new Date();
   const currentMonth = currentDate.getMonth() + 1;
+  const currentYear = currentDate.getFullYear();
 
   const endDateString = date.split(" - ")[1];
-  const [endMonth] = endDateString.split(".");
+  const [endMonth, endYear] = endDateString.split(".");
 
-  return currentMonth <= parseInt(endMonth, 10);
+  const endMonthNum = parseInt(endMonth, 10);
+  const endYearNum = parseInt(endYear, 10);
+
+  if (
+    currentYear < endYearNum ||
+    (currentYear === endYearNum && currentMonth <= endMonthNum)
+  ) {
+    return true;
+  }
+
+  return false;
 };
